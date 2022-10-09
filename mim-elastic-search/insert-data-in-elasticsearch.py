e84fc3d72858d677a9c80ac1c50fc9f85913f5c5
fhdufhdu <fhdufhdu@gmail.com>
2022-10-09 21:31:46 +0900

    엘라스틱 서치 데이터 삽입 코드 최신화

--- mim-elastic-search/insert-data-in-elasticsearch.py
+++ mim-elastic-search/insert-data-in-elasticsearch.py
@@ -3,52 +3,54 @@ from tqdm import tqdm
 from elasticsearch import Elasticsearch
 import json
 
-# es = Elasticsearch("localhost:9200/")
+es = Elasticsearch("http://localhost:9200/")
 
-# es.indices.delete(index="movie", ignore=[400, 404])
+es.indices.delete(index="movie", ignore=[400, 404])
 
-# es.indices.create(
-#     index='movie',
-#     body={
-#         "settings": {
-#             "index": {
-#                 "analysis": {
-#                     "tokenizer": "nori_tokenizer"
-#                 }
-#             }
-#         }
-#     }
-# )
-
-
-# es = Elasticsearch("http://localhost:9200/")
-# URL = "./final_data.json"
-# with open(URL, "r") as json_file:
-#     json_data = json.load(json_file)
-# body = ""
-# count = 0
-# for j in tqdm(json_data['data']):
-#     if (j['title'] == '검은 사제들'):
-#         print(j['title'])
-#     count += 1
-#     body = body + json.dumps({"index": {"_index": 'movie'}}) + '\n'
-#     body = body + json.dumps(j, ensure_ascii=False) + '\n'
-
-#     if((count % 100 == 0) or (count == len(json_data['data']))):
-#         es.bulk(body)
-#         print(str(count) + " 데이터 입력")
-#         es.indices.refresh(index="movie")
-#         body = ""
+es.indices.create(
+    index='movie',
+    body={
+        "settings": {
+            "index": {
+                "analysis": {
+                    "tokenizer": "nori_tokenizer"
+                }
+            }
+        }
+    }
+)
 
 
-# print("done")
+es = Elasticsearch("http://localhost:9200/")
+URL = "./final_data.json"
+with open(URL, "r") as json_file:
+    json_data = json.load(json_file)
+body = ""
+count = 0
+for j in tqdm(json_data['data']):
+    if (j['title'] == '검은 사제들'):
+        print(j['title'])
+    count += 1
+    # body = body + json.dumps({"index": {"_index": 'movie'}}) + '\n'
+    # body = body + json.dumps(j, ensure_ascii=False) + '\n'
+    es.index(index="movie", body=j)
+
+    # if((count % 100 == 0) or (count == len(json_data['data']))):
+        #es.bulk(body)
+        # es.index(index="movie", body=j)
+        #print(str(count) + " 데이터 입력")
+        #es.indices.refresh(index="movie")
+        #body = ""
+
+
+print("done")
 
 
 try:
     es = Elasticsearch("http://localhost:9200/")
     docs = es.search(index='movie',
                      body={
-                         "size": 100,
+                         "size": 1,
                          "query": {
                              "multi_match": {
                                  "query": "검은 사제들",
